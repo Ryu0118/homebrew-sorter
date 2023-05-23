033a99fd5f1a88f570bbf0a4d1af6c2d3f982b51
Ryu <87907656+Ryu0118@users.noreply.github.com>
2023-05-24 03:33:22 +0900

    first

---
+++ Formula/sorter.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Sorter < Formula
+  desc "Swift CLI tool to reorder enum cases, imports, etc."
+  homepage "https://github.com/Ryu0118/Sorter"
+  url "https://github.com/Ryu0118/Sorter/releases/download/0.0.2/sorter"
+  sha256 "3c6dbe03eb02b212b213d4adf6c70930cccbe1230f926a9f06e66fc21cc3000c"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "sorter"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test Kusa`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "#{bin}/sorter", "--version"
+  end
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# Ryu0118 Sorter
+
+## How do I install these formulae?
+
+`brew install ryu0118/sorter/<formula>`
+
+Or `brew tap ryu0118/sorter` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
